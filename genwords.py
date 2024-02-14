ca42ae6a1d152bcfc74fbbb6f811521b153ec83d
ibrahimfakhry <Ibrahim fakhrey@github.com>
2024-02-14 10:22:07 +0200

    Initial commit

---
+++ genwords.py
@@ -0,0 +1,18 @@
+import sys
+import random
+from string import printable
+from collections import defaultdict
+dictfile=sys.argv[1]
+
+words=defaultdict(set)
+
+with open(dictfile) as file:
+    wordlist=[w for w in file.read().split()]
+
+random.shuffle(wordlist)
+wordlist=list(filter(lambda w: all(c in printable for c in w),wordlist))
+wordlist=wordlist[:1300]
+for word in wordlist:
+    words[len(word).add(word)]
+with open("words.py","w") as file:
+    file.write("words={}".format(repr(dict(words))))
